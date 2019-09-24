ee9fa5367bdd7dc0fe775e42bd83eb3434fe9b7d
Zepeacedust <arnor0903@gmail.com>
2019-09-24 20:45:21 +0000

    slamma restinni inná

--- app.py
+++ app.py
@@ -3,17 +3,27 @@ from flask import Flask, render_template as rend
 app = Flask(__name__)
 with open("frettir.txt") as frett_file:
 	frettir = ast.literal_eval(frett_file.read())
-@app.route('/')
+with open("tolur.txt") as frett_file:
+	tolur = ast.literal_eval(frett_file.read())
+@app.route("/")
 def index():
+	return "<a href='/frettir'>fréttir</a><a href='/kennitala'>kennitölur</a>"
+
+@app.route('/frettir')
+def fretti():
 	return rend("frettir.html", title="index", frettir=frettir.keys())
 
 
 @app.route("/frettir/<frett>")
 def frett(frett):
 	if frett in frettir.keys():
-		return rend("frett.html", title=frettir[frett][0], content=frettir[frett][1], pic=frett)
+		return rend("frett.html", title=frettir[frett][0], content=frettir[frett][1], pic=frettir[frett][2])
 	else:
 		return page_not_found(None)
+
+@app.route("/kennitala/")
+def kennitolur():
+	return rend("tolur.html", title="index", frettir=[tolur[i] for i in tolur.keys()])
 @app.route("/kennitala/<tala>")
 def kennitala(tala):
 	return rend("tala.html", kennitala=tala, summa=sum(list(map(int, list(tala)))))
