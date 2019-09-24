f30075e2db6f8e3924777866017c47c6eb434a52
Zepeacedust <arnor0903@gmail.com>
2019-09-24 20:30:32 +0000

    shitt

---
+++ app.py
@@ -0,0 +1,26 @@
+import random, ast
+from flask import Flask, render_template as rend
+app = Flask(__name__)
+with open("frettir.txt") as frett_file:
+	frettir = ast.literal_eval(frett_file.read())
+@app.route('/')
+def index():
+	return rend("frettir.html", title="index", frettir=frettir.keys())
+
+
+@app.route("/frettir/<frett>")
+def frett(frett):
+	if frett in frettir.keys():
+		return rend("frett.html", title=frettir[frett][0], content=frettir[frett][1], pic=frett)
+	else:
+		return page_not_found(None)
+@app.route("/kennitala/<tala>")
+def kennitala(tala):
+	return rend("tala.html", kennitala=tala, summa=sum(list(map(int, list(tala)))))
+@app.errorhandler(404)
+def page_not_found(e):
+    return rend('404.html')
+
+
+if __name__ == "__main__":
+	app.run(debug=True)
